fd0f081366ac119560d644719be37063e2189681
NickAlbainy00 <NickAlbainy00@users.noreply.github.com>
2018-10-27 11:14:04 -0400

    Add project files.

---
+++ HackOHIO_2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HackOHIO_2018.Startup))]
+namespace HackOHIO_2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
